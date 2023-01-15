305d254a2fc1e65356d975a9fe080ec3afb0180b
sumi <sumipavib1302@gmail.com>
2023-01-15 12:45:21 +0530

    welcome to addressbook

---
+++ src/com/addressbookproblem/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.addressbookproblem;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+        System.out.println("Welcome in AddressBook system");
+        
+    }
+}
