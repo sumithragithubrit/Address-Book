ddee662914e1366c015dd180ea735b719f6ad033
sumi <sumipavib1302@gmail.com>
2023-01-15 14:38:44 +0530

    Added:Add contact to to address book

--- src/com/addressbookproblem/AddressBookMain.java
+++ src/com/addressbookproblem/AddressBookMain.java
@@ -3,6 +3,12 @@ package com.addressbookproblem;
 public class AddressBookMain {
 	public static void main(String[] args) {
         System.out.println("Welcome in AddressBook system");
+        Contact familyBook=new Contact();
+        System.out.println(familyBook);
+        AddressBook contact= new AddressBook();
         
+        contact.addContact();
+        
+
     }
 }
